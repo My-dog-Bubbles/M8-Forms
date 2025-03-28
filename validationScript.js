6bb95e4907f80d275ed97b421dabbcfe2039727a
Angel Carmichael <amcarmichael2@my.waketech.edu>
2025-03-28 13:46:21 -0400

    had to add test to make sure the code will if the vlause equals

--- validationScript.js
+++ validationScript.js
@@ -1,23 +1,31 @@
 const body = document.body
 
-// Retrieve the input field value
-const input = document.getElementById("inputField")
 
-// Prevent form from submitting (event listener)
-
-const submit = document.getElementsByTagName('button')
-console.log(submit)
-document.addEventListener('click', checker(){
-
-})
 
 // Regular expression pattern for alphanumeric input
 const alphaNum = /^[a-zA-Z0-9]+$/; 
 
-// JavaScript code for form validation
-// Check if the input value matches the pattern
-if (input === alphaNum){
-  // Valid input: display confirmation and submit the form
-} else {
-  // Invalid input: display error message
+// Prevent form from submitting (event listener)
+const submit = document.getElementById('submit')
+
+submit.addEventListener('click',alNumChecker);
+
+console.log(submit)
+
+function alNumChecker(){
+  // Retrieve the input field value
+  let input = document.getElementById("inputField")
+  
+  // JavaScript code for form validation
+  // Check if the input value matches the pattern
+  if (alphaNum.test(input.value)){
+    // Valid input: display confirmation and submit the form
+    input.setCustomValidity('this input is alphanumerical');
+  } else {
+    // Invalid input: display error message
+    input.setCustomValidity('This input is not alphanumerical');
+  }
 }
+
+
+
